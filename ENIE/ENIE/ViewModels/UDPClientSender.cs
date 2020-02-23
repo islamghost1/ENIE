0e5faacbe300025a417c534429ebcee6a42e16a1
Islem SISSAOUI <islem.sissaoui.dz@viacesi.fr>
2020-02-23 13:44:54 +0100

    Applying UDP sockets

---
+++ ENIE/ENIE/ViewModels/UDPClientSender.cs
@@ -0,0 +1,27 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENIE.ViewModels
+{
+   public class UDPClientSender
+    {
+        public async void UDP_ClientSender()
+        { 
+            var port = 11011;
+            var address = "127.0.0.1";
+
+            var client = new UdpSocketClient();
+
+            // convert our greeting message into a byte array
+            var msg = "HELLO WORLD";
+            var msgBytes = Encoding.UTF8.GetBytes(msg);
+
+            // send to address:port, 
+            // no guarantee that anyone is there 
+            // or that the message is delivered.
+            await client.SendToAsync(msgBytes, address, port);
+        }
+    }
+}
---
+++ ENIE_SERVER/ENIE_SERVER/ViewModel/UDPReceiver.cs
@@ -0,0 +1,29 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ENIE_SERVER.ViewModel
+{
+    public class UDPReceiver
+    {
+        public async void  UDP_Receiver()
+        {
+            var listenPort = 11011;
+            var receiver = new UdpSocketReceiver();
+
+            receiver.MessageReceived += (sender, args) =>
+            {
+                // get the remote endpoint details and convert the received data into a string
+                var from = String.Format("{0}:{1}", args.RemoteAddress, args.RemotePort);
+                var data = Encoding.UTF8.GetString(args.ByteData, 0, args.ByteData.Length);
+
+                Debug.WriteLine("{0} - {1}", from, data);
+            };
+
+            // listen for udp traffic on listenPort
+            await receiver.StartListeningAsync(listenPort);
+        }
+    }
+}
